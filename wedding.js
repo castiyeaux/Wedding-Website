a0221b402ab7328111bd2792659e772eef40f5d6
Andrea Castillo <andreacastillo@me.com>
2019-11-14 16:00:43 -0600

    More changes/experimentation

--- wedding.js
+++ wedding.js
@@ -2,7 +2,7 @@ $(function() {
 
     $(window).scroll(function() {
 
-        var mass = Math.max(20, 70-0.1*$(this).scrollTop()) + 'px';
+        var mass = Math.max(20, 50-0.1*$(this).scrollTop()) + 'px';
 
         $('#expandable').css({'font-size': mass, 'line-height': mass});
 
